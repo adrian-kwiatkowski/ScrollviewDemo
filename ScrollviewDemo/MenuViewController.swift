229e7679e715b4220160250cb66b7efc182f30e0
Adrian Kwiatkowski <adrian.kwiatkowski@qpony.pl>
2018-11-06 11:17:56 +0100

    InitiL setup

---
+++ ScrollviewDemo/MenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuViewController.swift
+//  ScrollviewDemo
+//
+//  Created by Adrian Kwiatkowski on 06/11/2018.
+//  Copyright © 2018 Adrian Kwiatkowski. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
---
+++ ScrollviewDemo/SelectionsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SelectionsViewController.swift
+//  ScrollviewDemo
+//
+//  Created by Adrian Kwiatkowski on 05/11/2018.
+//  Copyright © 2018 Adrian Kwiatkowski. All rights reserved.
+//
+
+import UIKit
+
+class SelectionsViewController: UIViewController {
+    
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "image.png"))
+        
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.black
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.flexibleWidth.rawValue | UIView.AutoresizingMask.flexibleHeight.rawValue)
+        scrollView.contentOffset = CGPoint(x: 100, y: 100)
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        
+        scrollView.delegate = self
+        setZoomScale()
+        setupGestureRecognizer()
+    }
+    
+    func setZoomScale() {
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        scrollView.minimumZoomScale = min(widthScale, heightScale)
+        scrollView.zoomScale = 1.0
+    }
+    
+    override func viewWillLayoutSubviews() {
+        setZoomScale()
+    }
+    
+}
+
+extension SelectionsViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+    }
+}
+
+//MARK: - gestures
+extension SelectionsViewController {
+    func setupGestureRecognizer() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(recognizer:)))
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func handleDoubleTap(recognizer: UITapGestureRecognizer) {
+        
+        if (scrollView.zoomScale > scrollView.minimumZoomScale) {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
+        }
+    }
+}

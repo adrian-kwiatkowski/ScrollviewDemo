088d527bfecf1b5f75a8e4e30811e246cc91638b
Adrian Kwiatkowski <adrian.kwiatkowski@qpony.pl>
2018-11-06 16:48:53 +0100

    add handling longpress, add random selections

--- ScrollviewDemo/SelectionsViewController.swift
+++ ScrollviewDemo/SelectionsViewController.swift
@@ -6,12 +6,14 @@
 //  Copyright © 2018 Adrian Kwiatkowski. All rights reserved.
 //
 
+var selectionsArray: [CGPoint] = []
+
 import UIKit
 
 class SelectionsViewController: UIViewController {
     
     lazy var scrollView: UIScrollView = UIScrollView(frame: view.bounds)
-    var imageView: UIImageView = UIImageView(image: UIImage(named: "image.png"))
+    var imageView: UIImageView = UIImageView(image: UIImage(named: "leaflet.jpg"))
     var selectionsView: UIView = UIView()
     var containerView: UIView = UIView()
 
@@ -20,6 +22,8 @@ class SelectionsViewController: UIViewController {
         setupSelectionsView()
         setupContainerView()
         setupScrollView()
+        
+        drawSelections()
     }
     
     func setupSelectionsView() {
@@ -41,7 +45,7 @@ class SelectionsViewController: UIViewController {
         scrollView.addSubview(containerView)
         
         setZoomScale()
-        setupGestureRecognizer()
+        setupGestureRecognizers()
         
         view.addSubview(scrollView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -58,13 +62,45 @@ class SelectionsViewController: UIViewController {
         let heightScale = scrollViewSize.height / containerViewSize.height
         
         scrollView.minimumZoomScale = min(widthScale, heightScale)
-        scrollView.zoomScale = 1.0
+        scrollView.maximumZoomScale = 2.0 / UIScreen.main.scale
+        scrollView.zoomScale = scrollView.minimumZoomScale
     }
     
     override func viewWillLayoutSubviews() {
         setZoomScale()
     }
     
+    fileprivate func addSelection() {
+        
+    }
+    
+    fileprivate func drawSelections() {
+        clearSelectionsView()
+        
+        selectionsArray.enumerated().forEach { (index, point) in
+            let imageNumber = (index % 4) + 1
+            let imageName = "selection\(imageNumber).png"
+            
+            if let image = UIImage(named: imageName) {
+                let selectionImageView = UIImageView(image: image)
+                
+                let selectionWidth = image.size.width * 2.5
+                let selectionHeight = image.size.height * 2.5
+                let selectionXCoordinate = point.x - (selectionWidth / 2)
+                let selectionYCoordinate = point.y - (selectionHeight / 2)
+                
+                selectionImageView.frame = CGRect(x: selectionXCoordinate, y: selectionYCoordinate, width: selectionWidth, height: selectionHeight)
+                
+                selectionsView.addSubview(selectionImageView)
+            }
+        }
+    }
+    
+    fileprivate func clearSelectionsView() {
+        selectionsView.subviews.forEach { (subview) in
+            subview.removeFromSuperview()
+        }
+    }
 }
 
 extension SelectionsViewController: UIScrollViewDelegate {
@@ -75,20 +111,23 @@ extension SelectionsViewController: UIScrollViewDelegate {
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         let containerViewSize = containerView.frame.size
         let scrollViewSize = scrollView.bounds.size
-        
+
         let verticalPadding = containerViewSize.height < scrollViewSize.height ? (scrollViewSize.height - containerViewSize.height) / 2 : 0
         let horizontalPadding = containerViewSize.width < scrollViewSize.width ? (scrollViewSize.width - containerViewSize.width) / 2 : 0
-        
+
         scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
     }
 }
 
 //MARK: - gestures
 extension SelectionsViewController {
-    func setupGestureRecognizer() {
+    func setupGestureRecognizers() {
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(recognizer:)))
         doubleTap.numberOfTapsRequired = 2
-        scrollView.addGestureRecognizer(doubleTap)
+        selectionsView.addGestureRecognizer(doubleTap)
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        selectionsView.addGestureRecognizer(longPress)
     }
     
     @objc func handleDoubleTap(recognizer: UITapGestureRecognizer) {
@@ -98,5 +137,14 @@ extension SelectionsViewController {
         } else {
             scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
         }
+    }
+    
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer) {
+        guard recognizer.state == .began else { return }
+        
+        let longPressedCenter = recognizer.location(in: selectionsView)
+        
+        selectionsArray.append(longPressedCenter)
+        drawSelections()
     }
 }
